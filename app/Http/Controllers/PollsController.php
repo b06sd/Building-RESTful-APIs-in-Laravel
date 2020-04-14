1a24f4ee417cab055dd223f1ed7c0f596d700d71
b06sd <dotundenton@gmail.com>
2020-04-14 18:39:25 +0100

    Implemented BREAD functions

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -32,4 +32,16 @@ class PollsController extends Controller
         $poll = Poll::create($request->all());
         return response()->json($poll, 201);
     }
+
+    public function update(Request $request, Poll $poll)
+    {
+        $poll->update($request->all());
+        return response()->json($poll, 200);
+    }
+
+    public function delete(Request $request, Poll $poll)
+    {
+        $poll->delete();
+        return response()->json(null, 204);
+    }
 }
